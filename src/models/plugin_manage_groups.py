a9f7ae9687ab0c72f26a004678a38b9d49e920b8
zvolsky <zvolsky@seznam.cz>
2016-01-25 09:58:49 +0100

    table definitions -repair

--- src/models/plugin_manage_groups.py
+++ src/models/plugin_manage_groups.py
@@ -4,20 +4,20 @@
 # Items saved as single 'group' with 'item's can be loaded into single python dictionary with keys 'item's.
 
 db.define_table('plugin_config_set',
-    Field('group', length=16),
+    Field('grp', length=16),
     Field('item', length=16),
-    Field('group_separate', 'boolean'),
+    Field('grp_separate', 'boolean'),
     Field('user_separate', 'boolean'),
-    Field('label', length=92),
-    Field('comment', 'text'),
-    format='%(group)s - %(item)s - %(label)s'
+    Field('txt_label', length=92),
+    Field('txt_comment', 'text'),
+    format='%(grp)s - %(item)s - %(txt_label)s'
     )
 
 db.define_table('plugin_config',
     Field('auth_group_id', db.auth_group),
     Field('auth_user_id', db.auth_user),
-    Field('group', length=16),
-    Field('item', length=16),
-    Field('content', length=256),
-    format='%(group)s - %(item)s'
+    Field('grp', length=16),
+    Field('dict_key', length=16),
+    Field('dict_value', length=256),
+    format='%(grp)s - %(dict_key)s'
     )
