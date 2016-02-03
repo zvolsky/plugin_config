50d1812499dca3176e7d3c2523798044711619cf
zvolsky <zvolsky@seznam.cz>
2016-02-03 10:22:18 +0100

    new db structures, starting with main plugin page

--- src/models/plugin_config.py
+++ src/models/plugin_config.py
@@ -3,23 +3,32 @@
 # Web2py plugin to manage/save/load config items.
 # Items saved as single 'group' with 'item's can be loaded into single python dictionary with keys 'item's.
 
-db.define_table('plugin_config_set',
+db.define_table('plugin_config_grp',
     Field('grp', length=16),
-    Field('item', length=16),
-    Field('grp_separate', 'boolean'),
-    Field('user_separate', 'boolean'),
-    Field('txt_label', length=92),
-    Field('txt_comment', 'text'),
-    format='%(grp)s - %(item)s - %(txt_label)s'
+    Field('user_settings', 'boolean'),
+    format='%(grp)s'
     )
 
-db.define_table('plugin_config',
-    Field('auth_group_id', db.auth_group),
-    Field('auth_user_id', db.auth_user),
-    Field('grp', length=16),
+db.define_table('plugin_config_key',
+    Field('plugin_config_grp_id', db.plugin_config_grp),
     Field('dict_key', length=16),
+    format='%(dict_key)s'
+    )
+
+db.define_table('plugin_config_txt',
+    Field('plugin_config_grp_id', db.plugin_config_grp),
+    Field('plugin_config_key_id', db.plugin_config_key),
+    Field('lang', length=5),
+    Field('txt_label', length=92),
+    Field('txt_comment', 'text'),
+    format='%(txt_label)s'
+    )
+
+db.define_table('plugin_config_val',
+    Field('plugin_config_key_id', db.plugin_config_key),
+    Field('auth_user_id', db.auth_user),
     Field('dict_value', length=256),
-    format='%(grp)s - %(dict_key)s'
+    format='%(dict_value)s'
     )
 
 # Following cofiguration values are defaults.
@@ -31,10 +40,9 @@ def _():
     plugins = PluginManager('config',
             admin_group='admin',  # name of the admin group (all changes allowed for members). empty string to disable
                                   # in addition changes are enabled for members of following groups:
-                                  #     plugin_config_add - change configuration structure
-                                  #     plugin_config_set - change global and group settings (if group s. will be implemented)
-                                  #     plugin_config_group - change group setting (if group s. will be implemented)
-                                  #   everybody can change his/her own setting (if user s. will be implemented)
+                                  #     plugin_config_structure - change configuration structure
+                                  #     plugin_config_global - change global settings
+                                  #   everybody can change his/her own setting
             )
 
 plugin_manage_groups = _()
